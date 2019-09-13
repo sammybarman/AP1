67a40596cc30596f91ed0d6dc1c11e1a886d7ae3
Sammy <sammy@Sammys-MacBook-Pro.local>
2019-09-13 09:42:12 +0530

    Shifted From Foundation to Materialize

---
+++ static/js/init.js
@@ -0,0 +1,52 @@
+// $('.carousel.carousel-slider').carousel({
+//     fullWidth: true,
+//     indicators: true
+//   });
+//   var autoplay = true;
+//   setInterval(function() { if(autoplay) $('.carousel.carousel-slider').carousel('next'); }, 2000);
+//   $('.carousel.carousel-slider').hover(function(){ autoplay = false; },function(){ autoplay = true; });
+//
+// $(document).ready(function(){
+//     $('.parallax').parallax();
+//   });
+//
+// $(document).ready(function(){
+//       $('.fixed-action-btn').floatingActionButton();
+//
+//   });
+//
+// $(document).ready(function(){
+//
+//       $(window).scroll(function(){
+//
+//        if($(window).scrollTop()>20){
+//          $('nav').addClass('bg');
+//          $('.brand-logo').removeClass('black-text');
+//          $('.brand-logo').addClass('red-text');
+//        }else{
+//          $('nav').removeClass('bg');
+//          $('.brand-logo').removeClass('red-text');
+//          $('.brand-logo').addClass('black-text');
+//        }
+//     });
+//   });
+
+  // Sidenav Implementation
+// $(function() {
+//         $('.sidenav').sidenav();
+//     })
+//
+// $(document).ready(function(){
+//         $('.fixed-action-btn').floatingActionButton();
+//     });
+//
+// $(document).ready(function(){
+//         $('.tap-target').tapTarget();
+//     });
+//
+// $(document).ready(function(){
+//       	$('.materialboxed').materialbox();
+//       });
+// $(document).ready(function(){
+//         $('.scrollspy').scrollSpy();
+//       });
