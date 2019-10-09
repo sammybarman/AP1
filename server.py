5cf40e0834ad9b016c00ebdec0773e3bf446aa4c
Nikhil Narasimhan <nikhil.narasimhan99@gmail.com>
2019-10-10 01:02:26 +0530

    Added flask security imports

--- server.py
+++ server.py
@@ -1,8 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for
+import urllib.parse, os, subprocess, re, requests, sqlite3, csv, io, threading
+from flask_security import Security, login_required, \
+     SQLAlchemySessionUserDatastore, roles_accepted
+from flask_security.utils import hash_password
+from database import db_session, init_db
+from models import User, Role
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'secret string'
+app.config['SECURITY_PASSWORD_SALT'] = 'some arbitrary super secret string'
+app.config['SECURITY_TRACKABLE'] = True
 
-@app.route("/")
+# Setup Flask-Security
+user_datastore = SQLAlchemySessionUserDatastore(db_session,
+                                                User, Role)
+security = Security(app, user_datastore)
+
+@app.route("/index")
 def renderPage():
     return render_template("index.html")
 
